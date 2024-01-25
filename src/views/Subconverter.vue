0d6a53c6feb215e635b6ba257d7bbb735b78bfce
kogekiplay <w981612327@gmail.com>
2024-01-25 16:42:01 +0800

    update

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -4,7 +4,7 @@
       <el-col>
         <el-card>
           <div slot="header">
-            Subscription Converter
+            私房菜 | 订阅转换
             <svg-icon icon-class="github" style="margin-left: 20px" @click="goToProject" />
 
             <div style="display: inline-block; position:absolute; right: 20px">{{ backendVersion }}</div>
@@ -342,7 +342,7 @@ export default {
     };
   },
   created() {
-    document.title = "Subscription Converter";
+    document.title = "私房菜 | 订阅转换";
     this.isPC = this.$getOS().isPc;
 
     // 获取 url cache
